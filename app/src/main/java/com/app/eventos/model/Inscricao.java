c9f37e2af6a5ca30123e1f85dc1e9813f08cb0a6
Leonardo Salmento <salmento_@hotmail.com>
2018-07-18 00:53:30 -0300

    Adicionando o método para calcular o valor da inscrição

--- app/src/main/java/com/app/eventos/model/Inscricao.java
+++ app/src/main/java/com/app/eventos/model/Inscricao.java
@@ -11,6 +11,12 @@ class Inscricao {
     private Evento evento;
     private Date dataVencimento;
     private Usuario usuario;
+    private static int qtdPalestra;
+    private static int qtdMiniCurso;
+    private static int qtdMesaRedonda;
+    private static Double valorPalestra = 15.00;
+    private static Double valorMiniCurso = 20.00;
+    private static Double valorMesaRedonda = 30.00;
 
     public Inscricao(Double valorTotal, ArrayList<Atividade> atividades, Boolean inscricaoPaga, Evento evento, Date dataVencimento, Usuario usuario) {
         this.valorTotal = valorTotal;
@@ -67,5 +73,24 @@ class Inscricao {
 
     public void setUsuario(Usuario usuario) {
         this.usuario = usuario;
+    }
+
+    public void encontraQuantidadeTiposAtividade(ArrayList<Atividade> atividades){
+        qtdPalestra = 0;
+        qtdMiniCurso = 0;
+        qtdMesaRedonda = 0;
+
+        for (int i = 0; i < atividades.size(); i++){
+            if (atividades.get(i).getTipoAtividade().equals(TipoAtividade.PALESTRA)){
+                qtdPalestra ++;
+
+            }else if (atividades.get(i).getTipoAtividade().equals(TipoAtividade.MINICURSO)){
+                qtdMiniCurso ++;
+
+            }else if (atividades.get(i).getTipoAtividade().equals(TipoAtividade.MINICURSO)){
+                qtdMesaRedonda ++;
+            }
+        }
+
     }
 }
